7c8d81b055c14191cabea58deeff4cafbfb068c7
Larissa Mak <larissa.mak@student.unsw.edu.au>
2019-04-27 12:58:34 +1000

    two bot player works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { timingSafeEqual } from 'crypto';
 
   const PlayerCard = ({color, symbol}) => {
     const style = {
@@ -9,7 +10,6 @@ import './App.css';
   
     return(
     <div style = {style} className="player-card">
-      <h1>Player1</h1>
     </div>
   )
 }
@@ -21,14 +21,38 @@ class App extends Component {
     this.state = {}
   }
 
-  runGame = () => {
-    //Set states for P1 and P2's hand 
-    this.setState({
-      playerRed: this.symbols[Math.floor(Math.random() * 3)], 
-      playerBlue: this.symbols[Math.floor(Math.random() * 3)]
-    }) 
-    console.log("Red " + this.state.playerRed + " Blue " + this.state.playerBlue)
+  decideWinner = ()=> {
+    const {playerBlue, playerRed} = this.state //gets state's Blue and Red state
+    if(playerRed == playerBlue){
+      return "Draw!"
+    }
+    //playerRed Wins!
+    if((playerRed=="rock" && playerBlue=="scissors") || 
+    (playerRed=="paper" && playerBlue=="rock") ||
+    (playerRed=="scissors" && playerBlue=="paper")){
+      return "Player Red Won!"
+    }
+    return "Blue Player wins!"
 
+  }
+
+  runGame = () => {
+    //interval timer for drawing outcome every 1/10th second, run it 40 times
+    let counter = 0
+    let myInterval = setInterval(() => {
+      counter++
+      //Set states for P1 and P2's hand 
+      this.setState({
+        playerRed: this.symbols[Math.floor(Math.random() * 3)], 
+        playerBlue: this.symbols[Math.floor(Math.random() * 3)], 
+        winner: ""
+      }) 
+      //animation stops after 40 times 
+      if(counter > 40){
+        clearInterval(myInterval)
+        this.setState({winner: this.decideWinner()})
+      }
+    },50)
   }
 
   render() {
@@ -40,7 +64,7 @@ class App extends Component {
         <PlayerCard
           color="blue"
           symbol= {this.state.playerBlue}/>
-
+        <h1>{this.state.winner}</h1>
         <button onClick={this.runGame}>Run Game</button>
       </div>
     );
