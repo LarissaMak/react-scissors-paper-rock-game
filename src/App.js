679ab65ed90774d6630bf4aab225c21fd21e1c29
Larissa Mak <larissa.mak@student.unsw.edu.au>
2019-04-28 09:46:12 +1000

    Score and restart button done

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.symbols = ["rock", "paper", "scissors"]
-    this.state = {}
+    this.state = {
+      humanScore : 0, 
+      botScore : 0
+    }
   }
 
   decideWinner = ()=> {
@@ -30,14 +33,21 @@ class App extends Component {
     if((humanPlayer=="rock" && botPlayer=="scissors") || 
     (humanPlayer=="paper" && botPlayer=="rock") ||
     (humanPlayer=="scissors" && botPlayer=="paper")){
+      this.setState({
+        humanScore: this.state.humanScore + 1
+      })
       return "You Human Won!"
     }
+    this.setState({
+      botScore: this.state.botScore + 1
+    })
     return "Computer Rules the world!"
 
   }
 
   runGame = (el) => {
     let humanCard = el.target.value
+
     //interval timer for drawing outcome every 1/10th second, run it 40 times
     let counter = 0
     let myInterval = setInterval(() => {
@@ -56,6 +66,13 @@ class App extends Component {
     },50)
   }
 
+  restartGame = ()=> {
+    this.setState({
+      humanScore: 0,
+      botScore: 0
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -70,6 +87,9 @@ class App extends Component {
         <button onClick={this.runGame} value="paper">Paper</button>
         <button onClick={this.runGame} value="scissors">Scissors</button>
         <button onClick={this.runGame} value="rock">Rock</button>
+        <h1>My Score {this.state.humanScore}</h1>
+        <h1>Bot Score {this.state.botScore}</h1>
+        <button onClick={this.restartGame}>Restart</button>
 
       </div>
     );
